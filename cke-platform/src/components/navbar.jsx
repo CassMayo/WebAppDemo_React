ae189de9958d1ab3f17856147277231b1c6db426
Markus <markusevanger@gmail.com>
2024-03-26 13:08:07 +0100

    add routing and basic functional navbar

--- cke-platform/src/components/navbar.jsx
+++ cke-platform/src/components/navbar.jsx
@@ -1,13 +1,44 @@
 
 
+import './navbarStyles.css'
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+
 
 export default function NavBar() {
 
     return (
         <>
-            <h1>Hei</h1>
+            <nav className="nav">
+                <Link to="/" className="site-title"> Site Name</Link>
+                <ul>
+
+                    
+                    <li>
+                        <CustomLink to="/about">About</CustomLink>
+                    </li>
+
+                    <li>
+                        <CustomLink to="/pricing">Pricing</CustomLink>
+                    </li>
+
+                </ul>
+            </nav>
         </>
     )
 }
 
+
+function CustomLink({ to, children, ...props}){
+
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({path: resolvedPath.pathname, end: true})
+    
+    return (
+        <li className= { isActive ? "active" : "" }>
+            <Link to={to} {...props}>
+                {children}
+            </Link>
+        </li>
+    )
+}
 
