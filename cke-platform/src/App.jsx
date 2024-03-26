af1a2d63b8c9a570a072806e934c544eb1eb5008
Markus <markusevanger@gmail.com>
2024-03-26 12:22:21 +0100

    setup react

---
+++ cke-platform/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import NavBar from './components/navbar.jsx'
+
+
+
+function App() {
+
+  return (
+    <>
+      <NavBar></NavBar>
+    </>
+  )
+}
+
+export default App
---
+++ cke-platform/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+
+
+
+export default function NavBar() {
+
+    return (
+        <>
+            <h1>Hei</h1>
+        </>
+    )
+}
+
+
