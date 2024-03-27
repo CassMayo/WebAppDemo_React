054fbd50e7159a892bbba4a36fe3a7bd2261817c
Markus <markusevanger@gmail.com>
2024-03-27 13:32:57 +0100

    Change navbar to use CustomLink, allowing React Router to function

--- cke-platform/src/App.jsx
+++ cke-platform/src/App.jsx
@@ -10,7 +10,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
-import Pricing from './pages/Pricing.jsx';
+import Portfolio from './pages/Portfolio.jsx';
 
 function App() {
   return (
@@ -21,7 +21,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/portfolio" element={<Portfolio />} />
         </Routes>
       </div>
       <Footer /> 
--- cke-platform/src/components/Navbar/navbar.jsx
+++ cke-platform/src/components/Navbar/navbar.jsx
@@ -1,21 +1,24 @@
 
 
+import CustomLink from './CustomLink';
 import './navbarStyles.css'
-import { Link, useMatch, useResolvedPath } from "react-router-dom"
-
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
     return (
       <nav className="navbar">
           <Link to="/">
-        <img src="/images/ClearKarbon.PNG" alt="Clear Karbon Exchange Logo" className="navbar-logo" />
-        </Link>
+            <img src="/images/ClearKarbon.PNG" alt="Clear Karbon Exchange Logo" className="navbar-logo" />
+          </Link>
         <div className="navbar-links">
-          <a href="/about">About Us</a>
-          <a href="/portfolio">Portfolio</a>
-          <a href="/invest">Invest</a>
-          <a href="/team">The Team</a>
+
+          <CustomLink to="/about">About Us</CustomLink>
+          <CustomLink to="/portfolio">Portfolio</CustomLink>
+          <CustomLink to="/invest">Invest</CustomLink>
+          <CustomLink to="/team">The Team</CustomLink>
+          
           <button className="navbar-login">Login</button>
+        
         </div>
       </nav>
     );
