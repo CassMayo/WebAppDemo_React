05e71f6ff6965217b897ff4de6be1cb0437dd475
CassMayo <117642651+Cassandra-dev@users.noreply.github.com>
2024-03-27 21:09:49 +0100

    Small changes

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ Repository for webpage project
 Demoteamet:
 - Markus Evanger
 - Cassandra Averil Mayorga
+
+WIKI
+cd cke-platform
+npm run dev
+
+- Icon
+npm install @fortawesome/react-fontawesome @fortawesome/free-solid-svg-icons @fortawesome/fontawesome-svg-core
+
+- Font head
+npm install react-helmet
--- cke-platform/src/App.jsx
+++ cke-platform/src/App.jsx
@@ -7,6 +7,7 @@ import NavBar from './components/Navbar/navbar.jsx';
 import Footer from './components/Footer/Footer.jsx';
 
 import { Routes, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
@@ -16,6 +17,11 @@ function App() {
   return (
     <>
     <div className="App"> 
+    <Helmet>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap" rel="stylesheet" />
+            </Helmet>
       <NavBar />
       <div className="content"> 
         <Routes>
