8c1e8d45847549c999733704046888607375802e
CassMayo <117642651+Cassandra-dev@users.noreply.github.com>
2024-03-27 14:29:46 +0100

    Attributes fixes, reduce redundancy

--- cke-platform/src/components/Navbar/navbar.jsx
+++ cke-platform/src/components/Navbar/navbar.jsx
@@ -6,19 +6,19 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom"
 
 const Navbar = () => {
     return (
-      <nav className="navbar">
-          <Link to="/">
-        <img src="/images/ClearKarbon.PNG" alt="Clear Karbon Exchange Logo" className="navbar-logo" />
-        </Link>
-        <div className="navbar-links">
-          <a href="/about">About Us</a>
-          <a href="/portfolio">Portfolio</a>
-          <a href="/invest">Invest</a>
-          <a href="/team">The Team</a>
-          <button className="navbar-login">Login</button>
-        </div>
-      </nav>
+        <nav className="navbar">
+            <Link to="/">
+                <img src="/images/ClearKarbon.PNG" alt="Clear Karbon Exchange Logo" className="navbar-logo" />
+            </Link>
+            <div className="navbar-links">
+                <a href="/about">About Us</a>
+                <a href="/portfolio">Portfolio</a>
+                <a href="/invest">Invest</a>
+                <a href="/team">The Team</a>
+                <button className="navbar-login">Login</button>
+            </div>
+        </nav>
     );
-  };
-  
-  export default Navbar;
+};
+
+export default Navbar;
