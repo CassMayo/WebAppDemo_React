eed21801e9d8f60d0d646d52fc8aedf7bb5519dc
Markus <markusevanger@gmail.com>
2024-03-30 14:06:23 +0100

    add sorting and refactor dropdown components to be "more reusablee"

--- cke-platform/src/components/Portfolio/CardsContentSection/DropdownItem/DropdownItem.jsx
+++ cke-platform/src/components/Portfolio/CardsContentSection/DropdownItem/DropdownItem.jsx
@@ -3,9 +3,10 @@ import "./DropdownItem.css"
 
 export default function DropdownItem(props) {
     return (
-        <div className="dropdownItemContainer" onClick={() => props.addFilter(props.name)}>
+        <div className="dropdownItemContainer" onClick={() => props.doOnClick(props.name)}>
             <p>{props.name}</p>
-            {props.filter.includes(props.name) && <i class="fa-solid fa-check"></i>}
+
+            {/*props.filter.includes(props.name) && <i className="fa-solid fa-check"></i>*/}
         </div>
     )
 }
