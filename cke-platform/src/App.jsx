f4488adbc8b0d40c7fe06bf49d45716c250eeed6
Markus <markusevanger@gmail.com>
2024-03-31 23:35:13 +0200

    remove bootstrap from packages. Please run npm install again

--- cke-platform/src/App.jsx
+++ cke-platform/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
