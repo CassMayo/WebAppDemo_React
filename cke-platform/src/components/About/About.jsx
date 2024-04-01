d7da7f360796b0f5a40085479fc48a7ee050af80
CassMayo <117642651+Cassandra-dev@users.noreply.github.com>
2024-04-01 11:28:19 +0200

    about

--- cke-platform/src/components/About/About.jsx
+++ cke-platform/src/components/About/About.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./About.css";
+import Header from "./Header/Header";
 
 const About = () => {
     return (
         <div className="about-container">
-            <h1>About heiiii </h1>
+            <Header />
         </div>
     );
     }
---
+++ cke-platform/src/components/About/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Empowering change, one carbon credit at a time</h1>
+      <p>We are a collective of concerned students, united under a common cause: to combat climate change by addressing one of its root causes: carbon emissions. Our journey began with a shared realization that if we are to protect our planet for future generations, the time for action is now. If not us, who else? If not now, when else?</p>
+    </header>
+  );
+};
+
+export default Header;
