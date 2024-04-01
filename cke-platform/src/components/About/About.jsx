dcf797329cf6fd4be9ea2748361f2c9a9b7570af
CassMayo <117642651+Cassandra-dev@users.noreply.github.com>
2024-04-01 17:15:05 +0200

    ABOUT

--- cke-platform/src/components/About/About.jsx
+++ cke-platform/src/components/About/About.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./About.css";
 import Header from "./Header/Header";
+import Mission from "./Mission/Mission";
 
 export default function About() {
     return (
         <div className="about-container">
-            <Header />
+            <div className="section"><Header /></div>
+            <div className="section"><Mission /></div>
         </div>
     );
     }
