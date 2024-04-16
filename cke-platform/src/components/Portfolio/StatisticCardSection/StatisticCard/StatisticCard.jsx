9670571b58c3a5a47c178bc6717ff0b2c85bcf1a
Markus <markusevanger@gmail.com>
2024-04-17 00:10:22 +0200

    add counter ticking up for Tons of CO2 removed

--- cke-platform/src/components/Portfolio/StatisticCardSection/StatisticCard/StatisticCard.jsx
+++ cke-platform/src/components/Portfolio/StatisticCardSection/StatisticCard/StatisticCard.jsx
@@ -1,10 +1,13 @@
 import "./StatisticCard.css"
 
 export default function StatisticCard(props){
+
+
     return(
         <div className="statisticCard">
             <p className="title">{props.title}</p>
-            <h3 className="number">{props.number}</h3>
+            <h3 className="number">{props.number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}</h3> 
+            {/* ^^ Long as RegEx funksjon som formaterer 488332 til 488 332 (altså legger til mellomrom)*/}
         </div>
     )
 }
--- cke-platform/src/components/Portfolio/StatisticCardSection/StatisticCardSection.jsx
+++ cke-platform/src/components/Portfolio/StatisticCardSection/StatisticCardSection.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState, useCallback } from "react";
 import StatisticCard from "./StatisticCard/StatisticCard";
 import "./StatisticCardSection.css"
 
 export default function StatisticCardSection(){
+
+    const [tons, setTons] = useState(423861)
+
+    const incrementCounter = useCallback((amount) => {
+        setTons((prevTons) => prevTons + amount);
+      }, []);
+    
+      useEffect(() => {
+        incrementCounter(1);
+    
+        const intervalId = setInterval(() => {
+          incrementCounter(1);
+        }, 4000);
+        // Clean up the interval on component unmount
+        return () => clearInterval(intervalId);
+      }, [incrementCounter]);
+  
+
+
     return(
         <div className="statisticCardContainer">
         <StatisticCard title="Available projects" number="247"/>
         <StatisticCard title="Countries represented" number="133"/>
-        <StatisticCard title="Tonnes of CO2 removed" number="423 861"/>
+        <StatisticCard title="Tons of CO2 removed" number={tons}/>
         </div>
     )
 }
