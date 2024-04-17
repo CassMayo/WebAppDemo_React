37b0053e3a12aecdc1d91ab6a71e8392fc8f92c3
CassMayo <117642651+Cassandra-dev@users.noreply.github.com>
2024-04-17 19:39:35 +0200

    css fixes

--- cke-platform/src/components/Company/RegisterCredit/RegisterCredit.jsx
+++ cke-platform/src/components/Company/RegisterCredit/RegisterCredit.jsx
@@ -70,112 +70,114 @@ const RegisterNewCredits = ({ listingStartState, setActiveTab, isEditingListing,
   return (
     <div>
 
-      <div className='register-credits-form'> 
-      <form onSubmit={handleSubmit} className="form">
+      <div className='register-credits-form'>
+        <form onSubmit={handleSubmit} className="form">
 
-        <div className="input-group">
-          <label htmlFor="title">Title of listing</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            value={newListing.title}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
+          <div className="input-group">
+            <label htmlFor="title">Title of listing</label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              value={newListing.title}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
 
-        <div className="input-group">
-          <label htmlFor="title">Description</label>
-          <textarea
-            type="text"
-            id="description"
-            name="description"
-            value={newListing.description}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
+          <div className="input-group">
+            <label htmlFor="title">Description</label>
+            <textarea
+              type="text"
+              id="description"
+              name="description"
+              value={newListing.description}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
 
-        <div className="input-group">
-          <label htmlFor="numberOfCredits">Number of credits</label>
-          <input
-            type="number"
-            id="numberOfCredits"
-            name="credits"
-            value={newListing.credits}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
+          <div className="input-group">
+            <label htmlFor="numberOfCredits">Number of credits</label>
+            <input
+              type="number"
+              id="numberOfCredits"
+              name="credits"
+              value={newListing.credits}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
 
-        <div className="input-group">
-          <label htmlFor="pricePerCredit">Price per credit</label>
-          <input
-            type="number"
-            id="pricePerCredit"
-            name="price"
-            value={newListing.price}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-
-
-        <div className="input-group">
-          <label htmlFor="pricePerCredit">Location</label>
-          <input
-            type="text"
-            id="location"
-            name="location"
-            value={newListing.location}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-
-        <div className="input-group">
-          <label htmlFor="image url">Please paste an image URL</label>
-          <input
-            type="text"
-            id="image Url"
-            name="imageUrl"
-            value={newListing.imageUrl}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
+          <div className="input-group">
+            <label htmlFor="pricePerCredit">Price per credit</label>
+            <input
+              type="number"
+              id="pricePerCredit"
+              name="price"
+              value={newListing.price}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
 
 
-        <div className="input-group">
-          <label htmlFor="typeOfCredits">Type of credits</label>
-          <select
-            id="typeOfCredits"
-            name="type"
-            value={newListing.type}
-            onChange={handleInputChange}
-            required
-          >
-            <option value="">Choose Type of Credits</option>
-            {creditTypes.map((type, index) => (
-              <option key={index} value={type}>{type}</option>
-            ))}
-          </select>
-        </div>
+          <div className="input-group">
+            <label htmlFor="pricePerCredit">Location</label>
+            <input
+              type="text"
+              id="location"
+              name="location"
+              value={newListing.location}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="image url">Please paste an image URL</label>
+            <input
+              type="text"
+              id="image Url"
+              name="imageUrl"
+              value={newListing.imageUrl}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
 
 
-        <button type="submit" className="submit-button">Save</button>
+          <div className="input-group">
+            <label htmlFor="typeOfCredits">Type of credits</label>
+            <select
+              id="typeOfCredits"
+              name="type"
+              value={newListing.type}
+              onChange={handleInputChange}
+              required
+            >
+              <option value="">Choose Type of Credits</option>
+              {creditTypes.map((type, index) => (
+                <option key={index} value={type}>{type}</option>
+              ))}
+            </select>
+          </div>
 
 
 
 
+          <button type="submit" className="submit-button">Save</button>
 
 
-      </form>
 
-      <div className='imageDiv'>
-        <h4> Listing image </h4>
-          <img  className="image" src={newListing.imageUrl} alt={newListing.imageAlt} />
+
+
+
+        </form>
+
+        <div className='imageDiv'>
+          <h4> Listing image </h4>
+          <img className="image" src={newListing.imageUrl} alt={newListing.imageAlt} />
         </div>
       </div>
 
