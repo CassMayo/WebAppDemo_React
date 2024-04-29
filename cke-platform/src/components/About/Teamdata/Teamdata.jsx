b5f1d4d702fcd72069cc8bad9d529aae5ffb7d98
CassMayo <117642651+Cassandra-dev@users.noreply.github.com>
2024-04-29 12:45:48 +0200

    About us update

--- cke-platform/src/components/About/Teamdata/Teamdata.jsx
+++ cke-platform/src/components/About/Teamdata/Teamdata.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Teamcard from '../Teamcard/Teamcard';
 import './Teamdata.css';
 import teamMembers from './TeamMembers.json';
+import InternsData from '../Interns/InternsData.json';
 
 const LeadershipTeam = () => {
   return (
@@ -11,6 +12,14 @@ const LeadershipTeam = () => {
         {teamMembers.map(member => (
           <Teamcard key={member.id} {...member} />
         ))}
+      </div>
+
+      <h2>Our Interns</h2>
+      <div className="InternTeam">
+        {InternsData.map(member => (
+          <Teamcard key={member.id} {...member} />
+        ))}
+
       </div>
     </section>
   );
